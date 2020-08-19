6896bbc47424ef4bed317a0dfe4473b411285e6e
IamSolaSholzy01 <ogolagunju@gmail.com>
2020-08-19 12:38:45 +0100

    Corrections

--- resources/views/applicant/home.blade.php
+++ resources/views/applicant/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.applicant_view')
 
 @section('content')
-    <div>Welcome {{$user->firstname.' '.$user->lastname}}</div>
+    <div class="alert alert-success"><p>Welcome {{$user->firstname.' '.$user->lastname}}</p>
+        <p>Please Proceed to any of the pages to start/continue your application.</p>
+    </div>
 @endsection
