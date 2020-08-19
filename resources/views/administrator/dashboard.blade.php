4dd76414915bd4ce5367e6adc3225bd2aa698010
IamSolaSholzy01 <ogolagunju@gmail.com>
2020-08-19 13:10:56 +0100

    Changes

--- resources/views/administrator/dashboard.blade.php
+++ resources/views/administrator/dashboard.blade.php
@@ -43,24 +43,6 @@
                             <div class="card">
                                 <div class="card-heading p-4">
                                     <div class="mini-stat-icon float-right">
-                                        <i class="mdi mdi-briefcase-check bg-success text-white"></i>
-                                    </div>
-                                    <div>
-                                        <h5 class="font-16">Transactions</h5>
-                                    </div>
-                                    <h3 class="mt-4">&#8358; 73,265</h3>
-                                    <div class="progress mt-4" style="height: 4px;">
-                                        <div class="progress-bar bg-success" role="progressbar" style="width: 88%" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <!-- <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">88%</span></p> -->
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-6 col-xl-3">
-                            <div class="card">
-                                <div class="card-heading p-4">
-                                    <div class="mini-stat-icon float-right">
                                         <i class="mdi mdi-tag-text-outline bg-warning text-white"></i>
                                     </div>
                                     <div>
